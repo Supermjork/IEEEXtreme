a6f0f41462c038eac29d64effb216eb541f279f2
Supermjork <markehabaziz@gmail.com>
2024-07-08 16:51:44 +0300

    Installing a compiler should not have taken that long

---
+++ Codeforces/Sheet 1/basic_data_types.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num_int;
+    long long num_llng;
+    char c;
+    float num_f;
+
+    cin >> num_int >> num_llng >> c >> num_f;
+
+    cout << num_int << "\n" << num_llng << "\n" << c << "\n" << num_f << endl ;
+}
---
+++ Codeforces/Sheet 1/simple_calculator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    long long num_0, num_1;
+
+    cin >> num_0 >> num_1;
+
+    cout << num_0 << " + " << num_1 << " = " << num_0 + num_1 << "\n";
+    cout << num_0 << " * " << num_1 << " = " << num_0 * num_1 << "\n";
+    cout << num_0 << " - " << num_1 << " = " << num_0 - num_1 << endl;
+}
