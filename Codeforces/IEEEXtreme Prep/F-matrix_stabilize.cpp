ea04e5d6b61200014cf705fec7a84300d45a9f3c
Supermjork <markehabaziz@gmail.com>
2024-07-23 14:03:04 +0300

    This works, just slow

--- Codeforces/IEEEXtreme Prep/F-matrix_stabilize.cpp
+++ Codeforces/IEEEXtreme Prep/F-matrix_stabilize.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
-vector<vector<int>> populate_matrix(vector<vector<int>> matrix)
+vector<vector<int>> populate_matrix(vector<vector<int>>& matrix)
 {
     for (int i = 0; i < matrix.size(); i++)
     {
@@ -15,38 +16,100 @@ vector<vector<int>> populate_matrix(vector<vector<int>> matrix)
     return matrix;
 }
 
-void print_matrix(vector<vector<int>> matrix)
+void print_matrix(const vector<vector<int>>& matrix)
 {
     for (int i = 0; i < matrix.size(); i++)
     {
         for (int j = 0; j < matrix[i].size(); j++)
         {
-            if (j != matrix[i].size() - 1)
-            {
-                cout << matrix[i][j] << ' ';
-            }
-            else
-            {
-                cout << matrix[i][j];
-            }
+            cout << matrix[i][j] << ' ';
         }
+
         cout << endl;
     }
 }
 
+vector<int> grab_neighbours(vector<vector<int>> matrix, int row, int col)
+{
+    int row_lim = matrix.size();
+    int col_lim = matrix[0].size();
+
+    vector<int> neighbours;
+
+    if (row > 0)
+    {
+        neighbours.push_back(matrix[row - 1][col]);
+    }
+
+    if (row < row_lim - 1)
+    {
+        neighbours.push_back(matrix[row + 1][col]);
+    }
+
+    if (col > 0)
+    {
+        neighbours.push_back(matrix[row][col - 1]);
+    }
+
+    if (col < col_lim - 1)
+    {
+        neighbours.push_back(matrix[row][col + 1]);
+    }
+
+    return neighbours;
+}
+
+bool greater_than_neighbours(int current_cell, vector<int> neighbours)
+{
+    bool isGreater = false;
+
+    for (int i = 0; i < neighbours.size(); i++)
+    {
+        if (current_cell > neighbours[i])
+        {
+            isGreater = true;
+        }
+        else
+        {
+            isGreater = false;
+            break;
+        }
+    }
+
+    return isGreater;
+}
+
 int main()
 {
-    int rows, cols;
+    int rows, cols, tcases;
 
-    cin >> rows >> cols;
+    cin >> tcases;
 
-    vector<vector<int>> in_matrix(rows, vector<int>(cols));
+    while(tcases--)
+    {
+        bool unstable = true;
+        cin >> rows >> cols;
 
-    in_matrix = populate_matrix(in_matrix);
+        vector<vector<int>> in_matrix(rows, vector<int>(cols));
 
-    // Stabilisation Code
+        populate_matrix(in_matrix);
 
-    print_matrix(in_matrix);
+        // Stabilisation Code
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                vector<int> neighbours = grab_neighbours(in_matrix, i, j);
+
+                while (greater_than_neighbours(in_matrix[i][j], neighbours))
+                {
+                    in_matrix[i][j]--;
+                }
+            }
+        }
+
+        print_matrix(in_matrix);
+    }
 
     return 0;
 }
