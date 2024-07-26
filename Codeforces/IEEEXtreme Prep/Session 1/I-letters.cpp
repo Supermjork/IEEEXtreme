041fd5963c0864c78bb720041ee16ff5ececd115
Supermjork <markehabaziz@gmail.com>
2024-07-26 12:00:16 +0300

    Variable type beeg

--- Codeforces/IEEEXtreme Prep/Session 1/I-letters.cpp
+++ Codeforces/IEEEXtreme Prep/Session 1/I-letters.cpp
@@ -8,8 +8,8 @@ int main()
     int n, m;
     cin >> n >> m;
 
-    vector<int> room_dorm(n);
-    vector<int> letters(m);
+    vector<long long> room_dorm(n);
+    vector<long long> letters(m);
 
     cin >> room_dorm[0];
 
@@ -26,11 +26,23 @@ int main()
 
     for (int i = 0; i < m; i++)
     {
-        int dorm_idx = lower_bound(room_dorm.begin(), room_dorm.end(), letters[i]) - room_dorm.begin();
+        long long dorm_idx, room_no;
+
+        // Dorm Indexing
+        // Finding which dorm the letter may lie within
+        dorm_idx = lower_bound(room_dorm.begin(), room_dorm.end(), letters[i]) - room_dorm.begin();
 
         // Missing room offset
+        if (dorm_idx == 0)
+        {
+            room_no = letters[i];
+        }
+        else
+        {
+            room_no = letters[i] - room_dorm[dorm_idx - 1];
+        }
 
-        cout << dorm_idx + 1 << ' ';
+        cout << dorm_idx + 1 << ' ' << room_no << endl;
     }
 
     return 0;
