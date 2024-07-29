476aeacddb676506dc3b640e7c4593acc60da18d
Supermjork <markehabaziz@gmail.com>
2024-07-29 18:43:20 +0300

    Brain ded for today

---
+++ Codeforces/IEEEXtreme Prep/Session 1/D-scuza.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n_steps, n_questions;
+        cin >> n_steps >> n_questions;
+
+        vector<int> heights(n_steps);
+    }
+}
